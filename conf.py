7a485606b41301c4c59b35c121dcd18dd4db7c8a
Rollins Orlu <rollins@dtechghana.com>
2021-02-22 17:47:17 +0000

    Updated html logo path

--- conf.py
+++ conf.py
@@ -99,7 +99,7 @@ html_short_title = 'sERP School ERP documentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/serp-logo.png'
+html_logo = '_static/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
